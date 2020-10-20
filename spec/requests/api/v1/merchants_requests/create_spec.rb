c1993e03fe5cef44c156c8e31639d32ede90ba48
Norma Lopez <norma_l304@yahoo.com>
2020-10-20 01:48:49 -0600

    Change private method in merchants controller to not require merchant. Rails driver spec harness now passing

--- spec/requests/api/v1/merchants_requests/create_spec.rb
+++ spec/requests/api/v1/merchants_requests/create_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Merchant API" do
   it "can create a new merchant" do
-    merchant_params = ({
-                    name: 'Walmart'
-                  })
+    body = {
+      name: 'Walmart'
+    }
     headers = {"CONTENT_TYPE" => "application/json"}
 
-    post "/api/v1/merchants", headers: headers, params: JSON.generate(merchant: merchant_params)
+    post "/api/v1/merchants", headers: headers, params: JSON.generate(body)
 
     merchant = JSON.parse(response.body, symbolize_names: true)
 
     last_merchant = Merchant.last
 
     expect(response).to be_successful
-    expect(last_merchant.name).to eq(merchant_params[:name])
+    expect(last_merchant.name).to eq(body[:name])
 
     expect(merchant[:data]).to be_a(Hash)
 
