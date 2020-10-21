0a16be32c781a65407d2ca74b18671465055107d
Norma Lopez <norma_l304@yahoo.com>
2020-10-21 00:07:32 -0600

    Update items/merchant show endpoint to return single merchant object rather than array

--- app/controllers/api/v1/items/merchant_controller.rb
+++ app/controllers/api/v1/items/merchant_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Items::MerchantController < ApplicationController
   def show
     item = Item.find(params[:id])
-    merchant = Merchant.where(id: item.merchant_id)
+    merchant = Merchant.where(id: item.merchant_id).first
     render json: MerchantSerializer.new(merchant)
   end
 end
