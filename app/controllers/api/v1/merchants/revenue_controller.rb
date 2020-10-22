3e5e1ce28090611e738c65471bfd1283460a452a
Norma Lopez <norma_l304@yahoo.com>
2020-10-22 16:11:03 -0600

    Update Merchant.most_revenue() to take in params[:quantity] for limit

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def index
-    merchants = Merchant.most_revenue(limit)
+    merchants = Merchant.most_revenue(params[:quantity])
     render json: MerchantSerializer(merchants)
   end
 end
