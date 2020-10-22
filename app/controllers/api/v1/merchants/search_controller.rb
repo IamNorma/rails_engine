5a79914b563c8a7624a1d5e7223aef703d4256e0
Norma Lopez <norma_l304@yahoo.com>
2020-10-22 00:58:47 -0600

    Fix rubocop offenses; align elsif and end statements with if

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -14,10 +14,10 @@ class Api::V1::Merchants::SearchController < ApplicationController
     attribute = params.keys.first
     query = params[attribute]
     merchants = if attribute == 'name'
-                 Merchant.where("lower(#{attribute}) LIKE ?", "%#{query.downcase}%")
-               elsif attribute == 'created_at' || attribute == 'updated_at'
-                 Merchant.where("CAST(#{attribute} AS text) LIKE ?", "%#{query.downcase}%")
-               end
+                  Merchant.where("lower(#{attribute}) LIKE ?", "%#{query.downcase}%")
+                elsif attribute == 'created_at' || attribute == 'updated_at'
+                  Merchant.where("CAST(#{attribute} AS text) LIKE ?", "%#{query.downcase}%")
+                end
     render json: MerchantSerializer.new(merchants)
   end
 end
