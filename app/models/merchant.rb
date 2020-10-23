837565c6bcec52b24f0112534b5bc97b01a271ad
Norma Lopez <norma_l304@yahoo.com>
2020-10-22 19:42:41 -0600

    Add transactions relationship

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,15 @@ class Merchant < ApplicationRecord
 
   has_many :items, dependent: :destroy
   has_many :invoices, dependent: :destroy
+  has_many :transactions, through: :invoices
+
+  def self.most_revenue(limit)
+    quantity = limit.to_i
+    joins(invoices: [:invoice_items, :transactions])
+    .select('merchants.id, merchants.name, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue')
+    .where("invoices.status='shipped' AND transactions.result='success'")
+    .group(:id)
+    .order('revenue DESC')
+    .limit(quantity)
+  end
 end
