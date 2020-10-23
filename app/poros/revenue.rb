768e51c0b47741907062d4db876b480d1f21b05c
Norma Lopez <norma_l304@yahoo.com>
2020-10-23 01:16:07 -0600

    Add revenue poro with ::total_revenue

--- app/poros/revenue.rb
+++ app/poros/revenue.rb
@@ -1,8 +1,26 @@
 class Revenue
-  attr_reader :id, :total
+  attr_reader :id, :revenue
 
-  def initialize(id, total)
+  def initialize(revenue)
     @id = nil
-    @total = total
+    @revenue = revenue
+  end
+
+  def self.total_revenue(start_date, end_date)
+    total = Invoice.joins(:transactions, :invoice_items)
+      .where("invoices.status='shipped' AND transactions.result='success'")
+      .where(:created_at => start(start_date)..final(end_date))
+      .sum('invoice_items.unit_price * invoice_items.quantity')
+    Revenue.new(total)
+  end
+
+  private
+
+  def self.start(start_date)
+    start_date.to_date.beginning_of_day
+  end
+
+  def self.final(end_date)
+    end_date.to_date.end_of_day
   end
 end
