8c491f6dccd00f5967748e817e54b2239278dc37
Norma Lopez <norma_l304@yahoo.com>
2020-10-22 22:37:47 -0600

    Change index action to user Revenue poro and serializer instead of Merchant model and merchant serializer

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RevenueController < ApplicationController
   def index
-    total_revenue = Merchant.total_revenue(params[:start], params[:end])
-    render json: MerchantSerializer.new(total_revenue)
+    total_revenue = Revenue.total_revenue(params[:start], params[:end])
+    render json: RevenueSerializer.new(total_revenue)
   end
 end
